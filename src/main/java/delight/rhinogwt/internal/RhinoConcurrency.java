c717182085442b7a531077a49293ed2cadd950ab
mxro <mxro@nowhere.com>
2015-08-11 09:21:46 +1000

    Implementing Server-Side Procedures Autocommit Tue Aug 11 09:21:46 EST 2015

--- src/main/java/delight/rhinogwt/internal/RhinoConcurrency.java
+++ src/main/java/delight/rhinogwt/internal/RhinoConcurrency.java
@@ -2,4 +2,10 @@ package delight.rhinogwt.internal;
 
 @SuppressWarnings("all")
 public class RhinoConcurrency {
+  private Runnable executor;
+  
+  public String setTimeout(final Runnable fn, final int delay) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nType mismatch: cannot convert from void to String");
+  }
 }
--- src/main/xtend/delight/rhinogwt/internal/RhinoConcurrency.xtend
+++ src/main/xtend/delight/rhinogwt/internal/RhinoConcurrency.xtend
@@ -1,5 +1,16 @@
 package delight.rhinogwt.internal
 
+import java.util.Timer
+import java.util.TimerTask
+
 class RhinoConcurrency {
+	
+	Runnable executor;
+	
+	def String setTimeout(Runnable fn, int delay) {
+		
+		new Timer().schedule([], delay)
+		
+	} 
 	
 }

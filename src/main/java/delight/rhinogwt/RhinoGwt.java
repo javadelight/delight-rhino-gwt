4248b7d604b69210781a991f767b80899a22f446
mxro <mxro@nowhere.com>
2015-08-11 09:15:46 +1000

    Implementing Server-Side Procedures Autocommit Tue Aug 11 09:15:46 EST 2015

--- src/main/java/delight/rhinogwt/RhinoGwt.java
+++ src/main/java/delight/rhinogwt/RhinoGwt.java
@@ -1,5 +1,13 @@
 package delight.rhinogwt;
 
+import delight.rhinosandox.RhinoSandbox;
+
 @SuppressWarnings("all")
 public class RhinoGwt {
+  /**
+   * Add a basic GWT runtime environment to the global scope of the provided sandbox.
+   */
+  public Object injectGwtRuntimeEnvironment(final RhinoSandbox sandbox) {
+    return null;
+  }
 }
--- src/main/xtend/delight/rhinogwt/RhinoGwt.xtend
+++ src/main/xtend/delight/rhinogwt/RhinoGwt.xtend
@@ -1,5 +1,14 @@
 package delight.rhinogwt
 
+import delight.rhinosandox.RhinoSandbox
+
 class RhinoGwt {
+	
+	/**
+	 * Add a basic GWT runtime environment to the global scope of the provided sandbox.
+	 */
+	def injectGwtRuntimeEnvironment(RhinoSandbox sandbox) {
+		
+	}
 	
 }

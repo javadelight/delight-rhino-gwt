6206436d23ba892cb48c40163996d47ae7b2a0c3
mxro <mxro@nowhere.com>
2015-08-11 09:16:46 +1000

    Implementing Server-Side Procedures Autocommit Tue Aug 11 09:16:46 EST 2015

--- src/main/java/delight/rhinogwt/RhinoGwt.java
+++ src/main/java/delight/rhinogwt/RhinoGwt.java
@@ -1,13 +1,76 @@
 package delight.rhinogwt;
 
+import delight.rhinogwt.internal.RhinoConsole;
 import delight.rhinosandox.RhinoSandbox;
+import org.eclipse.xtend2.lib.StringConcatenation;
 
 @SuppressWarnings("all")
 public class RhinoGwt {
   /**
    * Add a basic GWT runtime environment to the global scope of the provided sandbox.
    */
-  public Object injectGwtRuntimeEnvironment(final RhinoSandbox sandbox) {
-    return null;
+  public Object injectGwtRuntimeEnvironment(final RhinoSandbox sandbox, final Runnable operationsRunner) {
+    Object _xblockexpression = null;
+    {
+      RhinoConsole _rhinoConsole = new RhinoConsole();
+      sandbox.inject("console", _rhinoConsole);
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.newLine();
+      _builder.append("var navigator = {");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("userAgent: \"gecko1_8\"");
+      _builder.newLine();
+      _builder.append("};");
+      _builder.newLine();
+      _builder.newLine();
+      _builder.append("var $wnd = {");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("alert: function(msg) {");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("console.log(msg);");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("},");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("setTimeout: setTimeout,");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("setInterval: setInterval,");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("clearTimeout: clearTimeout,");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("clearInterval: clearInterval,");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("location: {");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("hostname: \"localhost\"\t");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("}");
+      _builder.newLine();
+      _builder.append("};");
+      _builder.newLine();
+      _builder.newLine();
+      _builder.append("var window = $wnd;");
+      _builder.newLine();
+      _builder.newLine();
+      _builder.append("var $doc = {");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.newLine();
+      _builder.append("};");
+      _builder.newLine();
+      _builder.newLine();
+      _xblockexpression = sandbox.evalWithGlobalScope(_builder.toString());
+    }
+    return _xblockexpression;
   }
 }
--- src/main/xtend/delight/rhinogwt/RhinoGwt.xtend
+++ src/main/xtend/delight/rhinogwt/RhinoGwt.xtend
@@ -1,5 +1,6 @@
 package delight.rhinogwt
 
+import delight.rhinogwt.internal.RhinoConsole
 import delight.rhinosandox.RhinoSandbox
 
 class RhinoGwt {
@@ -7,8 +8,38 @@ class RhinoGwt {
 	/**
 	 * Add a basic GWT runtime environment to the global scope of the provided sandbox.
 	 */
-	def injectGwtRuntimeEnvironment(RhinoSandbox sandbox) {
+	def injectGwtRuntimeEnvironment(RhinoSandbox sandbox, Runnable operationsRunner) {
+		sandbox.inject("console", new RhinoConsole)
 		
+		// set up environment for GWT
+		sandbox.evalWithGlobalScope('''
+			
+			var navigator = {
+				userAgent: "gecko1_8"
+			};
+			
+			var $wnd = {
+				alert: function(msg) {
+					console.log(msg);
+				},
+				setTimeout: setTimeout,
+				setInterval: setInterval,
+				clearTimeout: clearTimeout,
+				clearInterval: clearInterval,
+				location: {
+					hostname: "localhost"	
+				}
+			};
+			
+			var window = $wnd;
+			
+			var $doc = {
+				
+			};
+			
+		''')
+		
+		// set up timers
 	}
 	
 }

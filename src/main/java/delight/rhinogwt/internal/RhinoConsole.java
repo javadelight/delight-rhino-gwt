3a44f31761acd0bd83f32221b95ae76683ecefd0
mxro <mxro@nowhere.com>
2017-05-14 10:21:24 +1000

    Recompiling Java from XTend source files Autocommit Sun May 14 10:21:24 AEST 2017

--- src/main/java/delight/rhinogwt/internal/RhinoConsole.java
+++ src/main/java/delight/rhinogwt/internal/RhinoConsole.java
@@ -5,6 +5,7 @@ import org.eclipse.xtext.xbase.lib.InputOutput;
 @SuppressWarnings("all")
 public final class RhinoConsole {
   public void log(final Object o) {
-    InputOutput.<String>println(o.toString());
+    String _string = o.toString();
+    InputOutput.<String>println(_string);
   }
 }
